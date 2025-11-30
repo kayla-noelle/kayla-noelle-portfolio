a5c092cdf1439487fad14bcef39f7e79a541c05b
kayla-noelle <knchavez14@gmail.com>
2025-11-30 08:51:37 -0800

    more color changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,16 @@ module.exports = {
         accent: '#01BAEF',       // accent color (buttons / links)
         customGreen:'#20BF55',
       },
+      keyframes: {
+          gradientWave: {
+            '0%': { backgroundPosition: '0% 50%' },
+            '50%': { backgroundPosition: '100% 50%' },
+            '100%': { backgroundPosition: '0% 50%' },
+          },
+        },
+        animation: {
+          gradientWave: 'gradientWave 6s ease infinite',
+        }
     },
   },
   plugins: [],
