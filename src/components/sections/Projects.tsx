4cf9b71f8db2ff9c1cacbf01096d2177418a244a
kayla-noelle <knchavez14@gmail.com>
2026-08-11 13:30:19 -0700

    tubby todd added under works

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -20,6 +20,19 @@ export default function Projects() {
                 </span>
               ))}
             </div>
+            <div
+              className="p-6 rounded-2xl hover:-translate-y-1 transition-all border border-[#0d0a07]"
+            >
+              <h3 className="text-xl font-bold mb-2">
+                <a href="https://tubbytodd.com/" target="_blank" rel="noopener noreferrer" className="btn-border-reveal inline-flex items-center gap-1 text-sm md:text-base">Tubby Todd<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg></a>
+              </h3>
+              <p className="font-inter text-[#1c1c1c] mb-4">Contributed to Tubby Todd's e-commerce experience</p>
+               {["Shopify", "Liquid", "JavaScript"].map((tech, key) => (
+                <span key={key} className="inline-block bg-[#0d0a07] font-inter font-semibold text-white px-3 py-1 rounded-full text-sm mr-2 mb-2">
+                  {tech}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </RevealOnScroll>
